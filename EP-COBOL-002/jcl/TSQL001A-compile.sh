a6dda8fcb1fa8157f13ed5a379db27ce4d07e4ec
SoyKevin111 <kevinstevennn768xd@gmail.com>
2026-08-30 19:44:19 +0000

    CONFIGURACION PARA OTRO SUBMODULO Y EJECUTAR .SQB CON SQL Y SIN SQL(CBL)

--- EP-COBOL-002/jcl/TSQL001A-compile.sh
+++ EP-COBOL-002/jcl/TSQL001A-compile.sh
@@ -5,7 +5,14 @@ PGM=TSQL001A
 
 # Sources (.sqb). Main program FIRST, then subprograms.
 SRC_MAIN=TSQL001AC
-SRC_SUBS="TSQL001AC1"
+SRC_SUB1="TSQL001AC1"
+SRC_SUB2="TSQL001AC2"
+
+# Sources with EXEC SQL (need the GixSQL preprocessor)
+SRC_ESQL="$SRC_MAIN $SRC_SUB1"
+# Plain COBOL sources (no EXEC SQL): go straight to cobc.
+# TSQL001AC2 uses SD/MERGE and has no SQL; gixpp cannot parse SD.
+SRC_PLAIN="$SRC_SUB2"
 
 # GixSQL Libraries
 GIXSQL_HOME="/usr"
@@ -17,7 +24,7 @@ COBCOPY="../cpy"
 SQLCOPY="$GIXSQL_HOME/share/gixsql/copy"
 
 # All sources: main first, then subprograms
-SRC_ALL="$SRC_MAIN $SRC_SUBS"
+SRC_ALL="$SRC_MAIN $SRC_SUB1 $SRC_SUB2"
 
 # Remove old versions
 rm -f ../bin/$PGM
@@ -25,9 +32,14 @@ for SRC in $SRC_ALL; do
   rm -f ../tcbl/$SRC.cbl
 done
 
-# GixSQL Prep and Bind
-for SRC in $SRC_ALL; do
+# GixSQL Prep and Bind (only sources that contain EXEC SQL)
+for SRC in $SRC_ESQL; do
   gixpp -e -S -I $SQLCOPY -I $COBCOPY -i ../cbl/$SRC.sqb -o ../tcbl/$SRC.cbl
+done
+
+# Plain COBOL sources: no preprocessing, just hand them to cobc as-is
+for SRC in $SRC_PLAIN; do
+  cp ../cbl/$SRC.sqb ../tcbl/$SRC.cbl
 done
 
 # Pause to check the results
